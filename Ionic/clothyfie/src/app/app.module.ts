452b5b5c01cc45143eb10c4dd399b60f6ae5db88
Dharanz <dharanm387@gmail.com>
2018-09-01 23:10:04 +0530

    Project NavBar

--- Ionic/clothyfie/src/app/app.module.ts
+++ Ionic/clothyfie/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsersComponent } from './components/users/users.component';
+
+var appRoute: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'users', component: UsersComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    UsersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
