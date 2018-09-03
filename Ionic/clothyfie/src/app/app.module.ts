1fd0d978dc5cd7bea0a66c6d2b7e86bf5b7b2809
Dharanz <dharanm387@gmail.com>
2018-09-03 21:59:14 +0530

    Clothyfie Navbar

--- Ionic/clothyfie/src/app/app.module.ts
+++ Ionic/clothyfie/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { UsersComponent } from './components/users/users.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 var appRoute: Routes = [
   { path: '', component: HomeComponent},
@@ -21,7 +22,8 @@ var appRoute: Routes = [
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoute)
+    RouterModule.forRoot(appRoute),
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Ionic/clothyfie/src/app/components/home/home.component.ts
+++ Ionic/clothyfie/src/app/components/home/home.component.ts
@@ -1,12 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [NgbCarouselConfig]
 })
 export class HomeComponent implements OnInit {
 
+  images = [
+  '../../../assets/carousalImages/img1.jpg', 
+  '../../../assets/carousalImages/img2.jpg',
+  '../../../assets/carousalImages/img3.jpeg',
+  '../../../assets/carousalImages/img4.jpeg'
+]
   constructor() { }
 
   ngOnInit() {
